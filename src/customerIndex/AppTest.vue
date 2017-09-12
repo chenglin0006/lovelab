c6126a67356ca12bc368a569b5052a2017812d6c
chenglin02 <chenglin02@dianping.com>
2017-09-12 14:08:13 +0800

    update

--- src/customerIndex/AppTest.vue
+++ src/customerIndex/AppTest.vue
@@ -48,7 +48,7 @@ export default {
                 recommand: obj
             }).then(
                 respone => {
-                   this.getUserListFun();
+                   this.getRecommandListFun();
                 },
                 respone => {
 
